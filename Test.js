3dfe5f1f0f8103bdc01e7dd4177ae2328817531b
Andrew Blair <silverspeed132@gmail.com>
2022-03-26 19:18:35 -0500

    Add files via upload

--- Test.js
+++ Test.js
@@ -1,3 +1,14 @@
+/*EncounterMaker
+Description:
+This program currently takes a given input, and displays an array of JS objects representing DnD 5e enemies to create a potential encounter.
+
+Known Bugs: 
+- Lines 106-118: numbers are assigned properly, but because of this, the sum of the enemyValues array indices may be considerably lower or higher than xpRange
+    - Possible Fix: have enemyValueArray pull only viable enemyXpAmnts (that are less than calcRange) from calcRange for all but the last enemy
+        - Wouldn't completely fix the bug, but would mitigate variance
+        - Would also need a way to lower the value of the difference lost when the last number is rounded
+*/
+
 
 //Array containing enemy JS objects
 const enemyArray = [
@@ -9,7 +20,16 @@ const enemyArray = [
     {name: "orc war chief", armorClass: 16, health: 93, strength: 4, dex: 1, con: 4, intel: 0, wis: 0, cha: 3, xpAmnt: 1100},
     {name: "allosaurus", armorClass: 13, health: 51, strength: 4, dex: 1, con: 3, intel: -4, wis: 1, cha: -3, xpAmnt: 450},
     {name: "hill giant", armorClass: 13, health: 105, strength: 5, dex: -1, con: 4, intel: -3, wis: -1, cha: -2, xpAmnt: 1800},
-    {name: "awakened shrub", armorClass: 9, health: 10, strength: -4, dex: -1, con: 0, intel: 0, wis: 0, cha: -2, xpAmnt: 0}
+    {name: "awakened shrub", armorClass: 9, health: 10, strength: -4, dex: -1, con: 0, intel: 0, wis: 0, cha: -2, xpAmnt: 0},
+    {name: "giant fire beetle", armorClass: 13, health: 4, strength: -1, dex: 0, con: 1, intel: -5, wis: -2, cha: -4, xpAmnt: 0},
+    {name: "blood hawk", armorClass: 12, health: 7, strength: -2, dex: 2, con: 0, intel: -4, wis: 2, cha: -3, xpAmnt: 25},
+    {name: "skeleton", armorClass: 13, health: 13, strength: 0, dex: 2, con: 2, intel: -2, wis: -1, cha: -3, xpAmnt: 50},
+    {name: "orc", armorClass: 13, health: 15, strength: 3, dex: 1, con: 3, intel: -2, wis: 0, cha: 0, xpAmnt: 100},
+    {name: "animated armor", armorClass: 18, health: 33, strength: 2, dex: 0, con: 1, intel: -5, wis: 1, cha: -5, xpAmnt: 200},
+    {name: "gargoyle", armorClass: 15, health: 52, strength: 2, dex: 0, con: 3, intel: -2, wis: 0, cha: -2, xpAmnt: 450},
+    {name: "hell hound", armorClass: 15, health: 45, strength: 3, dex: 1, con: 2, intel: -2, wis: 1, cha: -2, xpAmnt: 700},
+    {name: "banshee", armorClass: 12, health: 58, strength: -5, dex: 2, con: 0, intel: 1, wis: 0, cha: 3, xpAmnt: 1100},
+    {name: "earth elemental", armorClass: 17, health: 126, strength: 5, dex: -1, con: 5, intel: -3, wis: 0, cha: -3, xpAmnt: 1800}
 ] 
 
 function encounterMaker(numOfPlayers, diff, enemyAmnt, playerLvls){
@@ -56,19 +76,25 @@ function encounterMaker(numOfPlayers, diff, enemyAmnt, playerLvls){
             enemyCount = 1;
             break;
         case "small":
-            enemyCount = Math.floor(Math.random()*2) + 2;
+            enemyCount = 2;
+            xpRange /= 1.5
             break;
         case "medium":
-            enemyCount = Math.floor(Math.random()*3) + 4;
+            enemyCount = Math.floor(Math.random()*4) + 3;
+            xpRange /= 2
             break;
         case "large":
             enemyCount = Math.floor(Math.random()*4) + 7;
+            xpRange /= 2.5
             break;
         case "many":
             enemyCount = Math.floor(Math.random()*4) + 11;
+            xpRange /= 3
             break;
         case "horde":
-            enemyCount = xpNeeded / 100;
+            if(xpRange / 100 > 15){
+                enemyCount = xpRange / 100;
+            } else enemyCount = 15;
             break;
     }
 
@@ -76,7 +102,8 @@ function encounterMaker(numOfPlayers, diff, enemyAmnt, playerLvls){
     let enemyValues = enemyValueArray(xpRange, enemyCount);
 
     //Contains an array of possible enemy XP Values and creates 2 variables for calculations
-    let enemyXpAmnts = [0, 25, 50, 100, 200, 450, 700, 1100, 1800, 2300, 2900, 3900, 5000, 5900, 7200, 8400, 10000, 11500, 13000, 15000, 18000, 20000, 22000, 25000, 33000, 41000, 50000, 62000, 155000]
+    let enemyXpAmnts = [0, 25, 50, 100, 200, 450, 700, 1100, 1800, 2300, 2900, 3900, 5000, 5900, 7200,
+         8400, 10000, 11500, 13000, 15000, 18000, 20000, 22000, 25000, 33000, 41000, 50000, 62000, 155000]
     let lowerXpAmnt = 0, higherXpAmnt = 0;
 
     //Loops through the enemyValues array and finds which index of enemyXpAmnts it is closest to, and reassigns it to that number.
@@ -93,6 +120,8 @@ function encounterMaker(numOfPlayers, diff, enemyAmnt, playerLvls){
         } else {enemyValues[i] = lowerXpAmnt}
     }
 
+    //creates an array of JS objects from enemyArray... filters enemyArray for enemies that match the index at enemyValues, then randomly selects one of those enemies
+    //and pushes it to encounterArray
     let encounterArray = []
     for (let i = 0; i < enemyValues.length; i++){
         let tempArray = enemyArray.filter(obj => {
@@ -121,4 +150,4 @@ function enemyValueArray (range, enemyNum){
     return randArr;
 }
 
-encounterMaker(4, "hard", "small", [4,3,5,3]);
+encounterMaker(4, "hard", "medium", [4,3,5,3]);
